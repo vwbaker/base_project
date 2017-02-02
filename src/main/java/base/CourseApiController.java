fb3530460449871221bd9b7bf09005b9b3647dda
vwbaker <toriiii37@gmail.com>
2017-02-01 17:02:32 -0800

    Added posts.html and working related controllers to base_project repo

--- src/main/java/base/CourseApiController.java
+++ src/main/java/base/CourseApiController.java
@@ -1,10 +1,18 @@
 package base;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
 @RestController
 @RequestMapping("/course")
 public class CourseApiController {
--- src/main/java/base/PostApiController.java
+++ src/main/java/base/PostApiController.java
@@ -2,6 +2,7 @@ package base;
 
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/post")
 public class PostApiController {
