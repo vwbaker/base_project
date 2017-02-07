e537860a9d359363fd362e7334c1c95473a5612b
Devon Martin <devmart10@gmail.com>
2017-02-07 15:46:57 -0800

    Big commit. Adds support for Bootstrap, updates Angular stuff

--- src/main/java/base/Post.java
+++ src/main/java/base/Post.java
@@ -3,17 +3,20 @@ package base;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Post implements Serializable {
-
+public class Post implements Serializable, Comparable<Post> {
 
     private Long id;
+    private String timestamp;
     private String message;
 
     public Post (){};
 
     public Post(String message) {
         this.message = message;
+        this.timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date());
     }
 
     public Long getId() {
@@ -23,6 +26,14 @@ public class Post implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+    
+    public String getTimestamp() {
+    	return timestamp;
+    }
+    
+    public void setTimestamp(String timestamp) {
+    	this.timestamp = timestamp;
+    }
 
     public String getMessage() {
         return message;
@@ -31,4 +42,9 @@ public class Post implements Serializable {
     public void setMessage(String message) {
         this.message = message;
     }
+
+	@Override
+	public int compareTo(Post o) {
+		return timestamp.compareTo(o.getTimestamp());
+	}
 }
